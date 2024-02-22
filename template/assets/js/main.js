66d4916c9b502ab85bdc63fa64a69efaf281542d
ChrissQAng <goodomen@outlook.de>
2024-02-22 17:32:07 +0100

    ugly

--- template/assets/js/main.js
+++ template/assets/js/main.js
@@ -23,12 +23,72 @@ const weatherData = (data) => {
             // console.log(weatherData);
             console.log(weatherData);
             // --- output in html
+            // location
+            let location = document.createElement("p");
+            location.textContent = weatherData.name;
+            output.appendChild(location);
             console.log(weatherData.name);
+            // coordinates
+            let coordinates = document.createElement("p");
+            coordinates.textContent = `Breitengrad: ${weatherData.coord.lat.toFixed(
+              1
+            )} Längengrad: ${weatherData.coord.lon.toFixed(1)}`;
+            output.appendChild(coordinates);
+            console.log(weatherData.coord.lon);
+            console.log(weatherData.coord.lat);
+            // clouds
+            let clouds = document.createElement("p");
+            clouds.textContent = weatherData.clouds.all;
+            output.appendChild(clouds);
+            console.log(weatherData.clouds.all);
+            // time
+            let time = document.createElement("p");
+
+            let myDate = new Date();
+            time.textContent = myDate.toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            });
+            output.appendChild(time);
             console.log(weatherData.dt);
+            // date
+            let date = document.createElement("p");
+
+            let date01 = new Date();
+            date.textContent = date01.toLocaleDateString();
+            output.appendChild(date);
+            console.log({ date01 });
+
+            // temerature
+            let temp = document.createElement("p");
+            temp.textContent = weatherData.main.temp.toFixed(1);
+            // time.setAttribute("class", ) - Klasse vergeben
+            output.appendChild(temp);
             console.log(weatherData.main.temp);
-            console.log(weatherData.sys.sunset);
-            console.log(weatherData.sys.sunrise);
+            // wind
+            let wind = document.createElement("p");
+            wind.textContent = weatherData.wind.speed;
+            output.appendChild(wind);
             console.log(weatherData.wind.speed);
+            // prespressure
+            let pressure = document.createElement("p");
+            pressure.textContent = weatherData.main.pressure;
+            output.appendChild(pressure);
+            console.log(weatherData.main.pressure);
+            // sunset
+            let sunset = document.createElement("p");
+            let timeTrans01 = weatherData.sys.sunset;
+            let mySunset = new Date(timeTrans01 * 1000);
+            sunset.textContent = mySunset.toLocaleTimeString();
+            output.appendChild(sunset);
+            console.log(weatherData.sys.sunset);
+            // sunrise
+            let sunrise = document.createElement("p");
+            let timeTrans02 = weatherData.sys.sunrise;
+            let mySunrise = new Date(timeTrans02 * 1000);
+            sunrise.textContent = mySunrise.toLocaleTimeString();
+            output.appendChild(sunrise);
+            console.log(weatherData.sys.sunrise);
           })
           .catch((err) =>
             console.error(
